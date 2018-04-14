80cf583458776cff17d53dbc995d9c2615b42847
Nguyễn Chánh Anh Tuấn <nca.tuan109@gmail.com>
2018-04-14 22:48:14 +0700

    ĐỌC VÀ XUẤT THÔNG TIN 1 SINH VIÊN

--- 1712874.cpp
+++ 1712874.cpp
@@ -9,7 +9,7 @@ typedef struct SINHVIEN{
 	wchar_t MSSV[8];
 	wchar_t hoVaTen[maxSize];
 	wchar_t khoa[maxSize];
-	int khoaTuyen;
+	wchar_t khoaTuyen[5];
 	wchar_t ngaySinh[11];
 	wchar_t hinhAnhCaNhan[maxSize];
 } SV;
@@ -17,13 +17,22 @@ typedef struct SINHVIEN{
 wchar_t* getPart(FILE* file, wchar_t* str){
 	int ch, leng = 0;
 	do{
-		ch = fgetc(file);
+		ch = fgetwc(file);
 		if (ch == ',' || ch == '\n' || ch == EOF) break;
 		str[leng] = ch;
 		leng++;
 	} while (1);
-	str[leng] = '\0';
+	str[leng] = L'\0';
 	return str;
+}
+
+void getSinhVien(FILE* file, SINHVIEN &x){
+	getPart(file, x.MSSV);
+	getPart(file, x.hoVaTen);
+	getPart(file, x.khoa);
+	getPart(file, x.khoaTuyen);
+	getPart(file, x.ngaySinh);
+	getPart(file, x.hinhAnhCaNhan);
 }
 
 void wmain(){
@@ -32,17 +41,16 @@ void wmain(){
 
 	SINHVIEN a;
 	FILE* fin, *fout;
-	fopen_s(&fin, "1712874.csv", "rt");
+	_wfopen_s(&fin, L"1712882.csv", L"rt,ccs=UTF-8");
 	if (!fin){
 		wprintf(L"Không thể đọc file!\n");
 	}
 	else{
-		getPart(fin, a.MSSV);
-		wprintf(L"MSSV: %ls", a.MSSV);
+		fseek(fin, 3, SEEK_SET);
+		getSinhVien(fin, a);
 		fclose(fin);
-		fopen_s(&fout, "fileout.csv", "wt");
-		fwprintf(fout, a.MSSV);
+		_wfopen_s(&fout, a.MSSV, L"wt,ccs=UTF-8");
+		fwprintf(fout, L"%ls", &a.MSSV);
 		fclose(fout);
 	}
-
 }
